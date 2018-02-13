70dad02e2446f1bdd2c3b4529a61b203ba331607
Solomon Xie <solomonxie@foxmail.com>
2018-02-13 17:53:12 +0800

    asf

--- git-push.py
+++ git-push.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import git
+
+import pdb; pdb.set_trace()
+
+repo = git.Repo.init('.')
+
+repo.git.status()
+
+
+remote = repo.remote()
+
+
+
+repo.git.status()
 
